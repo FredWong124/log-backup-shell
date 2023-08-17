2958c6e543ee06c1d9abd3fafdbc8a7630d15ba9
Fred <142284986+FredWong124@users.noreply.github.com>
2023-08-18 01:03:03 +0800

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#set dir that you want to back up
+srcDir="/etc/httpd/logs"
+
+#set dir that you want to be destination
+dstDir="/home/ec2-user/web_ser_log_backup"
+
+#set how many days you want to keep your backup
+keeping_days=30
+
+#will tar up the file under the source directory
+tar zcfp $dstDir/web_server_log_backup$(date +%Y%m%d).tar.gz $srcDir
+
+#delete the oldest file after file keeping day are reached
+expired_backup=$(ls -t $dstDir | tail -n +$((keeping_days+1)))
+for del in $expired_backup; do
+        rm -rf $dstDir/$del
+
+done
+
+
+
+echo "done"
+exit 0
+
